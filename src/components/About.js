4f1958d191a79cdbee9192de047bed67858b952f
+ <+>
2023-04-19 19:59:38 -0400

    check

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About(props) {
+  return (
+      <aside>
+          <img src={props.img} alt="blog logo"  />
+      </aside>
+  )
+  }
+export default About;
